8a17bf0aa39eba2955f0b19142d4c0b738791cce
LeilanNaeimi <leilan.n@gmail.com>
2023-12-09 09:39:33 +0300

    Add buttonClicked()

--- script.js
+++ script.js
@@ -1,51 +1,92 @@
 const btnRock = document.querySelector(".btn-rock");
 const btnPaper = document.querySelector(".btn-paper");
 const btnScissors = document.querySelector(".btn-scissors");
+const btnAgain = document.querySelector(".btn-again");
+const result = document.querySelector(".result");
+const result2 = document.querySelector(".result2");
+const result3 = document.querySelector(".result3");
 
-const selection = ["scissors", "paper", "rock"];
+let computerStr;
 
-function getComputerChoice() {
-  let computerSelection = Math.floor(Math.random() * 3);
-  console.log(computerSelection);
-  return computerSelection;
-}
+const init = function () {
+  result.innerHTML = "-";
+  computerStr = 0;
+};
 
-btnPaper.addEventListener("click", function () {
-  buttonClicked(1);
-});
-
-btnRock.addEventListener("click", function () {
-  buttonClicked(2);
+btnAgain.addEventListener("click", function () {
+  //set to default
+  init();
 });
 
 btnScissors.addEventListener("click", function () {
-  buttonClicked(0);
+  //Scissors == 0
+  buttonClicked(0, "Scissors");
 });
 
-function buttonClicked(btnNumber) {
-  console.log(btnNumber);
-  return btnNumber;
+btnPaper.addEventListener("click", function () {
+  //Paper == 1
+  buttonClicked(1, "Paper");
+});
+
+btnRock.addEventListener("click", function () {
+  //Rock == 2
+  buttonClicked(2, "Rock");
+});
+
+function buttonClicked(PlayerSelected, playerStr) {
+  const computerSelected = Math.floor(Math.random() * 3);
+
+  if (computerSelected === 0) computerStr = "Scissors";
+  else if (computerSelected === 1) computerStr = "Paper";
+  else if (computerSelected === 2) computerStr = "Rock";
+
+  console.log(PlayerSelected, computerSelected, playerStr, computerStr);
+
+  if (
+    (PlayerSelected === 0 && computerSelected === 0) ||
+    (PlayerSelected === 1 && computerSelected === 1) ||
+    (PlayerSelected === 2 && computerSelected === 2)
+  ) {
+    console.log("Tie!");
+  }
+
+  if (
+    (PlayerSelected === 0 && computerSelected === 1) ||
+    (PlayerSelected === 1 && computerSelected === 2) ||
+    (PlayerSelected === 2 && computerSelected === 0)
+  )
+    console.log(`You Win!, ${playerStr} beats ${computerStr}`);
+
+  if (
+    (PlayerSelected === 0 && computerSelected === 2) ||
+    (PlayerSelected === 1 && computerSelected === 0) ||
+    (PlayerSelected === 2 && computerSelected === 1)
+  )
+    console.log(`You Loose!, ${computerStr} beats ${playerStr}`);
+
+  /// you win
+  // if (btnNumber === 0 && computerSelected === 1) {
+  //   console.log("You Win!, Scissors beats Paper");
+  // }
+
+  // if (btnNumber === 1 && computerSelected === 2) {
+  //   console.log("You Win!, Paper beats Rock");
+  // }
+
+  // if (btnNumber === 2 && computerSelected === 0) {
+  //   console.log("You Win!, Rock beats Scissors");
+  // }
+
+  /// you Loose
+  // if (btnNumber === 0 && computerSelected === 2) {
+  //   console.log("You Loose!, Rock beats Scissors");
+  // }
+
+  // if (btnNumber === 1 && computerSelected === 0) {
+  //   console.log("You Loose!, Scissors beats Paper");
+  // }
+
+  // if (btnNumber === 2 && computerSelected === 1) {
+  //   console.log("You Loose!, Paper beats Rock");
+  // }
 }
-
-// function playRound(playerSelection, computerSelection) {
-// }
-
-console.log(getComputerChoice(), buttonClicked());
-// const playerSelection = buttonClicked();
-// const computerSelection = getComputerChoice();
-// console.log(
-//   `playerSelection: ${playerSelection} and
-//   computerSelection: ${computerSelection} `
-// );
-
-// console.log(playRound(playerSelection, computerSelection));
-
-/*
-Rock beats Scissors
-Scissors beats Paper
-Paper beats Rock
-
-You Lost the game to a computer!
-Tie!
-Win!
-*/
