b527ab8876a823ca4f4842b9ac2b5352a2a6adb2
LeilanNaeimi <leilan.n@gmail.com>
2023-12-09 16:13:26 +0300

    change js

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const btnPaper = document.querySelector(".btn-paper");
 const btnScissors = document.querySelector(".btn-scissors");
 const btnAgain = document.querySelector(".btn-again");
 const result = document.querySelector(".result");
-const result2 = document.querySelector(".result2");
-const result3 = document.querySelector(".result3");
+const choice = document.querySelector(".choice");
 
-let computerStr;
+let computerStr = "";
+let btnCounter = 0;
 
 const init = function () {
-  result.innerHTML = "-";
+  console.log("set to default");
+  result.innerHTML = "";
+  choice.innerHTML = "";
   computerStr = 0;
+  btnCounter = 0;
 };
 
 btnAgain.addEventListener("click", function () {
@@ -18,18 +21,22 @@ btnAgain.addEventListener("click", function () {
   init();
 });
 
+console.log("aaaaaaaaaaa" + btnCounter);
 btnScissors.addEventListener("click", function () {
   //Scissors == 0
+  btnCounter++;
   buttonClicked(0, "Scissors");
 });
 
 btnPaper.addEventListener("click", function () {
   //Paper == 1
+  btnCounter++;
   buttonClicked(1, "Paper");
 });
 
 btnRock.addEventListener("click", function () {
   //Rock == 2
+  btnCounter++;
   buttonClicked(2, "Rock");
 });
 
@@ -40,13 +47,21 @@ function buttonClicked(PlayerSelected, playerStr) {
   else if (computerSelected === 1) computerStr = "Paper";
   else if (computerSelected === 2) computerStr = "Rock";
 
-  console.log(PlayerSelected, computerSelected, playerStr, computerStr);
+  console.log(
+    btnCounter,
+    PlayerSelected,
+    computerSelected,
+    playerStr,
+    computerStr
+  );
 
   if (
     (PlayerSelected === 0 && computerSelected === 0) ||
     (PlayerSelected === 1 && computerSelected === 1) ||
     (PlayerSelected === 2 && computerSelected === 2)
   ) {
+    choice.innerHTML = `Your choice is ${playerStr} and computer choice is ${computerStr}`;
+    result.innerHTML = "Tie!";
     console.log("Tie!");
   }
 
@@ -54,16 +69,21 @@ function buttonClicked(PlayerSelected, playerStr) {
     (PlayerSelected === 0 && computerSelected === 1) ||
     (PlayerSelected === 1 && computerSelected === 2) ||
     (PlayerSelected === 2 && computerSelected === 0)
-  )
+  ) {
+    choice.innerHTML = `Your choice is ${playerStr} and computer choice is ${computerStr}`;
+    result.innerHTML = `You Win!, ${playerStr} beats ${computerStr}`;
     console.log(`You Win!, ${playerStr} beats ${computerStr}`);
+  }
 
   if (
     (PlayerSelected === 0 && computerSelected === 2) ||
     (PlayerSelected === 1 && computerSelected === 0) ||
     (PlayerSelected === 2 && computerSelected === 1)
-  )
+  ) {
+    choice.innerHTML = `Your choice is ${playerStr} and computer choice is ${computerStr}`;
+    result.innerHTML = `You Loose!, ${computerStr} beats ${playerStr}`;
     console.log(`You Loose!, ${computerStr} beats ${playerStr}`);
-
+  }
   /// you win
   // if (btnNumber === 0 && computerSelected === 1) {
   //   console.log("You Win!, Scissors beats Paper");
